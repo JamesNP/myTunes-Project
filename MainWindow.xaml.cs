80dafef07e5e0a8073b5d215c1b3371c499df1c0
JamesNP <91146135+JamesNP@users.noreply.github.com>
2021-10-30 22:54:07 -0500

    Add Drag and Drop
    Add ability to drag and drop songs to playlists. Problem with clicking to drag scroll bar after selecting an item

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,6 +27,10 @@ namespace MyPlayer
         private readonly MediaPlayer mediaPlayer;
         private readonly ObservableCollection<string> playlistNames;
         private readonly System.Data.DataTable songs;
+        private Point startPoint;
+
+        public bool songPlaying { get; set; }
+        public string dropPlaylist { get; set; }
         public MainWindow()
         {
             InitializeComponent();
@@ -72,19 +76,25 @@ namespace MyPlayer
 
         private void PlayCommand_Executed(object sender, RoutedEventArgs e)
         {
-            //This isn't working
-            //How do you get info like song Id out of the data grid?
+            //Gets data from selected row to play song
             var temp = songsDataGrid.SelectedItem as DataRowView;
             int songId = Convert.ToInt32(temp.Row.ItemArray[0]);
             Song s = musicRepo.GetSong(songId);
 
             mediaPlayer.Open(new Uri(s.Filename));
             mediaPlayer.Play();
+            songPlaying = true;
         }
 
         private void StopCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
+            songPlaying = false;
             mediaPlayer.Stop();
+        }
+
+        private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = songPlaying;
         }
 
         private void playMenuItem_Click(object sender, RoutedEventArgs e)
@@ -132,6 +142,68 @@ namespace MyPlayer
             }
         }
 
-        
+        private void songsDataGrid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Store the mouse position on left button click
+            startPoint = e.GetPosition(null);
+        }
+
+        private void songsDataGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+    
+
+            // Start the drag-drop if mouse has moved far enough
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                songsDataGrid.SelectedItems.Count > 0 &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                DataRowView rowView = songsDataGrid.SelectedItem as DataRowView;
+                int songId = Convert.ToInt32(rowView.Row.ItemArray[0]);
+                string textSongId = songId.ToString();
+                Console.WriteLine("Dragging song " + textSongId);
+
+                // Initiate dragging the text from the textbox
+                DragDrop.DoDragDrop(songsDataGrid, textSongId, DragDropEffects.Copy);
+            }
+        }
+
+        private void playlistListBox_DragOver(object sender, DragEventArgs e)
+        {
+            //By default, no drag and drop
+            e.Effects = DragDropEffects.None;
+
+            // If the DataObject contains string data, extract it
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                //Reads in song ID as an integer
+                int textSongId = Convert.ToInt32((string)e.Data.GetData(DataFormats.StringFormat));
+
+                Label playlist = sender as Label;
+                if (playlist != null)
+                {
+                    if (playlist.AllowDrop)
+                    {
+                        dropPlaylist = (string)playlist.Content;
+                        e.Effects = DragDropEffects.Copy;
+                    }
+                }
+            }
+        }
+
+        private void playlistListBox_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                //Reads in song ID as an integer
+                int songId = Convert.ToInt32((string)e.Data.GetData(DataFormats.StringFormat));
+
+                //Add song to playlist
+                musicRepo.AddSongToPlaylist(songId, dropPlaylist);
+            }
+        }
     }
 }
